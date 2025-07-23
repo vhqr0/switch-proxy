bc4cb914c478bd0021fbd3f51ee43afc75017008
vhqr0 <zq_cmd@163.com>
2025-07-23 23:30:06 +0800

    update

--- src/popup/index.jsx
+++ src/popup/index.jsx
@@ -4,11 +4,10 @@ import { getProxy, getStorage } from "../chrome";
 import App from "./App";
 import "./styles.css";
 
-const root = createRoot(document.getElementById("app"));
-
 async function main() {
   const proxy = await getProxy();
   const proxyList = await getStorage("proxyList");
+  const root = createRoot(document.getElementById("app"));
   root.render(<App initProxy={proxy} initProxyList={proxyList || []} />);
 }
 
